f841015c82198034d6cec0ba174dcf2657874caa
“HamzA <hamza.khokhar660@gmail.com>
2017-09-07 00:38:53 -0600

    initial commit

---
+++ src/main/java/com/msubanner/spring/msubannerspring/MsuBannerSpringApplication.java
@@ -0,0 +1,14 @@
+package com.msubanner.spring.msubannerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsuBannerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsuBannerSpringApplication.class, args);
+	}
+}
+
+//THis is a test
---
+++ src/test/java/com/msubanner/spring/msubannerspring/MsuBannerSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msubanner.spring.msubannerspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsuBannerSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
