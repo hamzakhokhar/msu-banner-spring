390913108d23d3b938efcc2aa3e22280131a68b4
“HamzA <hamza.khokhar660@gmail.com>
2017-09-15 15:26:09 -0600

    extended students controller and views

--- src/main/java/com/msubanner/spring/msubannerspring/StudentsController.java
+++ src/main/java/com/msubanner/spring/msubannerspring/StudentsController.java
@@ -1,32 +1,76 @@
 package com.msubanner.spring.msubannerspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class StudentsController {
 
-    @RequestMapping(value = "/students")
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @RequestMapping(value = "/students", method = RequestMethod.GET)
     public ModelAndView test() {
 
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("students");
-        modelAndView.addObject("key1", "value1");
-
-//        List<Book> bookList=
-//                Arrays.asList(
-//                        new Book("Java ve Yazılım Tasarımı", 35d, "Altuğ B. Altıntaş", true),
-//                        new Book("Java Mimarisiyle Kurumsal Çözümler", 23d, "Rahman Usta", true),
-//                        new Book("Veri Yapıları ve Algoritmalar", 40d, "Rifat Çölkesen", false),
-//                        new Book("Veri Yapıları ve Algoritmalar", 40d, "Rifat Çölkesen", false),
-//                        new Book("Veri Yapıları ve Algoritmalar", 40d, "Rifat Çölkesen", true),
-//                        new Book("Mobil Pazarlama - SoLoMo", 15d, "Kahraman-Pelin Arslan", false),
-//                        new Book("Mobil Pazarlama - SoLoMo", 15d, "Kahraman-Pelin Arslan", true));
-//
-//        modelAndView.addObject("bookList", bookList);
-
+        modelAndView.setViewName("students/students");
+        modelAndView.addObject("students", studentRepository.findAll());
         return modelAndView;
     }
+
+    //CREATE - GET
+    @RequestMapping(value = "/students/new", method = RequestMethod.GET)
+    public ModelAndView createStudent(){
+        return new ModelAndView("students/students.create", "student", new Student());
+    }
+
+    //CREATE - GET
+    @RequestMapping(value = "/students", method = RequestMethod.POST)
+    public String submitStudent(@Valid @ModelAttribute("student")Student student,
+                         BindingResult result, ModelMap model) {
+        if (result.hasErrors()) {
+            return "error";
+        }
+
+        studentRepository.save(student);
+
+        return "redirect:/students";
+    }
+
+    //READ - GET
+    @RequestMapping(value = "/students/{id}", method = RequestMethod.GET)
+    public ModelAndView redirectToGetStudent(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("students/student");
+        return modelAndView;
+    }
+
+    //UPDATE - POST
+    // ** possibily a redirect
+//    @RequestMapping(value = "/students/{id}/update", method = RequestMethod.POST)
+//    public ModelAndView redirectToUpdateStudent(){
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("students/students");
+//        return modelAndView;
+//    }
+
+    //DELETE - POST
+    // ** possibily a redirect
+//    @RequestMapping(value = "/students/{id}/delete", method = RequestMethod.POST)
+//    public ModelAndView redirectToStudent(){
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("students/students");
+//        return modelAndView;
+//    }
 
 }
