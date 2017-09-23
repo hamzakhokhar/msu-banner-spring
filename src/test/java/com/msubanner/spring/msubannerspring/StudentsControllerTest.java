ff3e0c60f6ec13b003d3569cbc863329ca9dadc1
DESKTOP-9TQCT86\thien <thienngonguyenle@gmail.com>
2017-09-22 23:34:36 -0600

    Added unit test

---
+++ src/test/java/com/msubanner/spring/msubannerspring/StudentsControllerTest.java
@@ -0,0 +1,38 @@
+package com.msubanner.spring.msubannerspring;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentsControllerTest {
+    @Test
+    public void getStudentsView() throws Exception {
+    // check to make sure the correct ModelAndView object is return
+    new StudentsController().getStudentsView();
+    }
+
+    @Test
+    public void getStudentInfoView() throws Exception {
+    }
+
+    @Test
+    public void getStudentEditView() throws Exception {
+    }
+
+    @Test
+    public void getNewStudentView() throws Exception {
+    }
+
+    @Test
+    public void submitStudent() throws Exception {
+    }
+
+    @Test
+    public void editStudent() throws Exception {
+    }
+
+    @Test
+    public void deleteStudent() throws Exception {
+    }
+
+}
