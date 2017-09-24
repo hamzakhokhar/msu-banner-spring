4875a8417a41c39d23672616a78d3b50c0e6f704
TarinCHorne <31256481+TarinCHorne@users.noreply.github.com>
2017-09-24 10:15:49 -0600

    Created all the needed files
    Section Class
    SectionRepository Interface
    SectionController Class: still WIP need to finish all html files first
    
    section.html
    sections.html
    section,edit,html
    sections.create.html
    
    Course and Professor Classes are temp

---
+++ src/main/java/com/msubanner/spring/msubannerspring/SectionController.java
@@ -0,0 +1,23 @@
+package com.msubanner.spring.msubannerspring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class SectionController {
+
+    @Autowired
+    private SectionRepository sectionRepository;
+
+}
