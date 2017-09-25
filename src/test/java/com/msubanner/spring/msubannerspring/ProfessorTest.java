927b3ef6590184653f9f9a0e76183a81e5c135af
Matt Kline <mkline6@msudenver.edu>
2017-09-24 20:20:49 -0600

    Fixed missing @Test tag
    Fixed missing tags

--- src/test/java/com/msubanner/spring/msubannerspring/ProfessorTest.java
+++ src/test/java/com/msubanner/spring/msubannerspring/ProfessorTest.java
@@ -6,40 +6,46 @@ import org.junit.Test;
 
 public class ProfessorTest {
 
-	@Test
-	public void StudentTest() {
+	@Test	
+	public void ProfessorTest() {
 		Professor professor = new Professor("First", "Last");
 		assertTrue(professor != null);
 	}
 
+	@Test
 	public void getFirstNameTest() {
 		Professor professor = new Professor("First", "Last");
 		assertTrue(professor.getFirstName() == "First");
 	}
-	
+
+	@Test
 	public void setFirstNameTest() {
 		Professor professor = new Professor("First", "Last");
 		professor.setFirstName("NewFirst");
 		assertTrue(professor.getFirstName() == "NewFirst");
-    }
+	}
 
+	@Test
     public void getLastNameTest() {
     	Professor professor = new Professor("First", "Last");
 		assertTrue(professor.getLastName() == "Last");
     }
 
+	@Test
     public void setLastNameTest() {
     	Professor professor = new Professor("First", "Last");
     	professor.setLastName("NewLast");
 		assertTrue(professor.getLastName() == "NewLast");
     }
 
+	@Test
     public void getIdTest() {
     	Professor professor = new Professor("First", "Last");
     	professor.setId((long) 123456);
 		assertTrue(professor.getId() == (long) 123456);
     }
 
+	@Test
     public void setIdTest() {
     	Professor professor = new Professor("First", "Last");
     	professor.setId((long) 123456);
