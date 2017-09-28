585f568230a43f4515ecb2fa299a25db55f311a2
Matt Kline <mkline6@msudenver.edu>
2017-09-28 17:45:59 -0600

    JavaDocs
    Created new javadocs for the project. Wrote JavaDocs for the professor
    class.

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"com.msubanner.spring.msubannerspring.professor"}]
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"com.msubanner.spring.msubannerspring.professor","l":"Professor"}]
