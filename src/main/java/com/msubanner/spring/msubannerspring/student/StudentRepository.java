7b4257d3e93ad3e2b299b39104600dfcaee1a1fe
Matt Kline <mkline6@msudenver.edu>
2017-09-28 18:49:39 -0600

    Javadocs
    javadocs on the repositories

--- src/main/java/com/msubanner/spring/msubannerspring/student/StudentRepository.java
+++ src/main/java/com/msubanner/spring/msubannerspring/student/StudentRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 //import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 //@RepositoryRestResource
+/**
+ * This class will implement the CRUD operations for use in the the student
+ * controllers on the webpage
+ */
 public interface StudentRepository extends CrudRepository<Student, Long> {
 
 }
