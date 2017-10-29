2bfd64a01e4a35911ee4b11f61ed689711b1994e
DESKTOP-9TQCT86\thien <thienngonguyenle@gmail.com>
2017-10-28 23:11:00 -0600

    Review course class

--- src/main/java/com/msubanner/spring/msubannerspring/course/Course.java
+++ src/main/java/com/msubanner/spring/msubannerspring/course/Course.java
@@ -96,18 +96,22 @@ public class Course {
     */
 
     public String getCourseDept() {
+
         return courseDept;
     }
 
     public void setCourseDept(String courseDept) {
+
         this.courseDept = courseDept;
     }
 
     public Long getCourseId() {
+
         return courseId;
     }
 
     public void setCourseId(Long courseId) {
+
         try {
             this.courseId = courseId;
         }
@@ -117,82 +121,102 @@ public class Course {
     }
 
     public String getCourseName() {
+
         return courseName;
     }
 
     public void setCourseName(String courseName) {
+
         this.courseName = courseName;
     }
 
     public int getCreditValue() {
+
         return creditValue;
     }
 
     public void setCreditValue(int creditValue) {
+
         this.creditValue = creditValue;
     }
 
     public String getLevel() {
+
         return level;
     }
 
     public void setLevel(String level) {
+
         this.level = level;
     }
 
     public String getBuilding() {
+
         return building;
     }
 
     public void setBuilding(String building) {
+
         this.building = building;
     }
 
     public String getRoom() {
+
         return room;
     }
 
     public void setRoom(String room) {
+
         this.room = room;
     }
 
     public String getTime() {
+
         return time;
     }
 
     public void setTime(String time) {
+
         this.time = time;
     }
 
     public int getMinSize() {
+
         return minSize;
     }
+
 
     public void setMinSize(int minSize) {
         this.minSize = minSize;
     }
 
     public int getMaxSize() {
+
         return maxSize;
     }
 
     public void setMaxSize(int maxSize) {
+
         this.maxSize = maxSize;
     }
 
     public String getDescription() {
+
         return description;
     }
 
     public void setDescription(String description) {
+
         this.description = description;
     }
 
     public String getLearningObjectives() {
+
         return learningObjectives;
     }
 
     public void setLearningObjectives(String learningObjectives) {
+        
         this.learningObjectives = learningObjectives;
     }
 }
