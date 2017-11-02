5e99d6ab0f3ecb4768318f5893e629a49d107156
DESKTOP-9TQCT86\thien <thienngonguyenle@gmail.com>
2017-11-01 19:48:44 -0600

    Add some code to semester class

--- src/main/java/com/msubanner/spring/msubannerspring/semester/Semester.java
+++ src/main/java/com/msubanner/spring/msubannerspring/semester/Semester.java
@@ -11,5 +11,51 @@ public class Semester {
     // testing commit real quick
 	//@id
     // @GeneratedValue(strategy = GenerationType.AUTO)
+    private String semester;
+    private Long startTime;
+    private Long endTime;
 
+    protected Semester(){};
+
+
+    public Semester(Long startTime, Long endTime) {
+
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+
+    public String getSemester() {
+        return semester;
+    }
+
+
+    public void setSemester( String semester) {
+
+        this.semester = semester;
+    }
+
+
+    public Long getStartTime() {
+
+        return startTime;
+    }
+
+
+    public void setStartTime(Long startTime)
+    {
+        this.startTime = startTime;
+    }
+
+
+    public Long getEndTime() {
+        return endTime;
+    }
+
+
+    public void setEndTime(Long endTime) {
+        this.endTime = endTime;
+    }
 }
+
+
