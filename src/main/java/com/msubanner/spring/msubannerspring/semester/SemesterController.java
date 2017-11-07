26fca6b4f9dfad4fe609ac865ae18955421410bd
“HamzA <hamza.khokhar660@gmail.com>
2017-11-07 10:21:33 -0700

    dont know whyy

--- src/main/java/com/msubanner/spring/msubannerspring/semester/SemesterController.java
+++ src/main/java/com/msubanner/spring/msubannerspring/semester/SemesterController.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.*;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
